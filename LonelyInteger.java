74d4c62dd7db30e4beab62698113813fca8f9e5f
Sruthi Gomathi <86158320+SruthiGomathi@users.noreply.github.com>
2022-07-02 23:17:58 +0530

    Hackerrank - 1 Week Interview Preparation Kit
    https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/

---
+++ LonelyInteger.java
@@ -0,0 +1,58 @@
+/*https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+//---------------------------------------------------------------------------------------------------------------------------------
+    public static int lonelyinteger(List<Integer> a) {
+    // Write your code here
+        List<Integer> counterList = new ArrayList<>();
+        for(int i=0;i<a.size();i++){
+            if(counterList.contains(a.get(i)))
+                counterList.remove(a.get(i));
+            else
+                counterList.add(a.get(i));
+        }
+        return counterList.get(0);
+    }
+//---------------------------------------------------------------------------------------------------------------------------------
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
